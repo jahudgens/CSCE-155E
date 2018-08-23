f2acbc688329694632aa86ed6f74e4fdcedf8e46
jahudgens <42652413+jahudgens@users.noreply.github.com>
2018-08-23 12:56:11 -0500

    Add files via upload
    A source file that outputs name and major.

---
+++ hello.c
@@ -0,0 +1,16 @@
+1 /**
+2 * Author: Jason Hudgens and Cheick Ndiaye
+3 * Date: 2018/8/21
+4 *
+5 * A simple hello world program in C
+6 *
+7 **/
+8 #include <stdlib.h>
+9 #include <stdio.h>
+10
+11 int main(int argc, char **argv) {
+12
+13 printf("Jason Hudgens Mechanical Engineer!\n");
+14
+15 return 0;
+}
